9d304271d66d07839574c55877703d34cc9295c3
L30STF <166737740+L30STF@users.noreply.github.com>
2025-04-08 21:36:00 -0300

    commit inicial

---
+++ src/main/java/com/SitemaRPG/SitemaRPG/SitemaRpgApplication.java
@@ -0,0 +1,13 @@
+package com.SitemaRPG.SitemaRPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SitemaRPG/SitemaRPG/SitemaRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SitemaRPG.SitemaRPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
